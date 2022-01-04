afb3b07e7eba6a048515e2e22eba86979adca32c
GitCosmicray <94951603+GitCosmicray@users.noreply.github.com>
2022-01-04 18:03:16 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "publicsubnetid" {
+    default = "subnet-0b067025c93c48433"
+}
+variable "privatesubnetid" {
+    default = "subnet-0112e39ae96f47298"
+}
+variable "keyname" {
+    default = "ohio_key"
+}
+variable "windows_security_group" {
+    default = "sg-0c4d1efb671c26929"
+}
+variable "linux_security_group" {
+    default = "sg-08431bb922f4b69af"
+}
